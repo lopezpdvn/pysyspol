8111e4bbad14f8bef3c04a8b358626e3a7b4eaf2
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-06-18 17:41:45 -0500

    `pysyspol.fs.rsync` -> `pysyspol.fs.rsync_multiple`

--- pysyspol/fs.py
+++ pysyspol/fs.py
@@ -2,28 +2,31 @@ import sys
 import os
 import logging
 from itertools import chain
-from subprocess import Popen, PIPE
+from subprocess import Popen
 
-def rsync(src, dst, opts, *dirs, rsync_path='rsync'):
-    if dirs:
-        report = []
-        for idir in dirs:
-            cmd = [rsync_path]
-            cmd.extend(opts)
-            idir = [f.rstrip(os.sep) for f in idir]
-            if len(idir) > 1:
-                exclude = idir[1:]
-                exclude_args = map(lambda x: ('--exclude', os.sep + x), exclude)
-                cmd.extend((chain(*exclude_args)))
-            isrc = os.path.join(src, idir[0]) + os.sep
-            idst = os.path.join(dst, idir[0])
-            cmd.extend((isrc, idst))
+def rsync_multiple(src, dst, opts, *paths, rsync_path='rsync'):
+    report = []
+    for ipaths in paths:
+        cmd = [rsync_path]
+        cmd.extend(opts)
+        path = ipaths[0]
+        if len(ipaths) > 1:
+            excludes = ipaths[1:]
+            excludes_args = map(lambda x: ('--exclude', os.sep + x), excludes)
+            cmd.extend((chain(*excludes_args)))
+        isrc = os.path.join(src, path)
+        if path.endswith(os.sep):
+            idst = os.path.join(dst, path).rstrip(os.sep)
+        else:
+            idst = os.path.join(dst, os.path.split(path)[0])
+        cmd.extend((isrc, idst))
 
-            with Popen(cmd, stdout=sys.stdout, stderr=sys.stderr) as proc:
-                proc.wait()
-                if proc.returncode:
-                    logging.warning(
-                            'rsync proccess `{0}` returned code {1}'.format(
-                                cmd, proc.returncode))
-                report.append([cmd, proc.returncode])
-        return report
+        with Popen(cmd, stdout=sys.stdout, stderr=sys.stderr) as proc:
+            proc.wait()
+            if proc.returncode:
+                logging.error(
+                        'rsync proccess `{0}` returned code {1}'.format(
+                            ' '.join(cmd), proc.returncode))
+            report.append((tuple(cmd), proc.returncode))
+
+    return tuple(report)
