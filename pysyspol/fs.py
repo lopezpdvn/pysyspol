2c07a5ecdebeb33b5048e481c8d10f8022abef10
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-06-21 01:45:46 -0500

    `pysyspol.fs.rsync_multiple` is now relative

--- pysyspol/fs.py
+++ pysyspol/fs.py
@@ -7,20 +7,17 @@ from subprocess import Popen
 def rsync_multiple(src, dst, opts, *paths, rsync_path='rsync'):
     report = []
     for ipaths in paths:
-        cmd = [rsync_path]
+        cmd = [rsync_path, '--relative']
         cmd.extend(opts)
         path = ipaths[0]
         if len(ipaths) > 1:
             excludes = ipaths[1:]
-            excludes_args = map(lambda x: ('--exclude', os.sep + x), excludes)
+            excludes_args = map(lambda x: ('--exclude', os.path.join(path, x)),
+                    excludes)
             cmd.extend((chain(*excludes_args)))
-        isrc = os.path.join(src, path)
-        if path.endswith(os.sep):
-            idst = os.path.join(dst, path).rstrip(os.sep)
-        else:
-            idst = os.path.join(dst, os.path.split(path)[0])
+        isrc = os.path.join(src, '.', path)
+        idst = dst.rstrip(os.sep)
         cmd.extend((isrc, idst))
-
         with Popen(cmd, stdout=sys.stdout, stderr=sys.stderr) as proc:
             proc.wait()
             if proc.returncode:
