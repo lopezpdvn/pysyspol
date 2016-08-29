2f375f8d5fafa2853486bd6282b7f29419047d0a
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-08-29 11:30:24 -0500

    Added function `pysyspol.util.getdir`

--- pysyspol/util.py
+++ pysyspol/util.py
@@ -1,9 +1,22 @@
+import os
 from random import SystemRandom
 from string import ascii_letters, digits
+from os.path import abspath, dirname
+from inspect import getfile
 
 ALPHA_NUMERIC_STR = ascii_letters + digits
 
 sys_random = SystemRandom()
 
 def random_alphanumeric_str(n):
-    return ''.join((sys_random.choice(ALPHA_NUMERIC_STR) for i in range(n)))
+    return ''.join(sys_random.choice(ALPHA_NUMERIC_STR) for i in range(n))
+
+def getdir(obj):
+    '''Return dir of file in which object is defined
+
+    Example:
+
+      import inspect
+      script_dir = getdir(inspect.currentframe())
+    '''
+    return dirname(abspath(getfile(obj))).rstrip(os.sep)
