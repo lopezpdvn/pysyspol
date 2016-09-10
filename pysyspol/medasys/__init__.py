fdcfeef871d2001d2062b2d5f76f46bed8329507
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-09-10 16:53:27 -0500

    Added `pysyspol.medasys.get_tagged_resources`

--- pysyspol/medasys/__init__.py
+++ pysyspol/medasys/__init__.py
@@ -8,3 +8,7 @@ def get_core_resources(core_resources_fp):
 def logging_config(msgfmt='[{0}]: %(message)s', prgname=None,
         level=logging.INFO):
     logging.basicConfig(format=msgfmt.format(prgname), level=level)
+
+def get_tagged_resources(resources, tags=()):
+    return (resource for tag in tags for resource in resources
+            if tag in resource['tags'])
