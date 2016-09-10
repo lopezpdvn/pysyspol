7bff05c4938ef995c255b159ce57356475abb028
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-09-10 16:28:55 -0500

    Added `pysyspol.medasys.logging_config`

--- pysyspol/medasys/__init__.py
+++ pysyspol/medasys/__init__.py
@@ -1,5 +1,10 @@
 import json
+import logging
 
 def get_core_resources(core_resources_fp):
     with open(core_resources_fp) as coref:
         return json.load(coref)
+
+def logging_config(msgfmt='[{0}]: %(message)s', prgname=None,
+        level=logging.INFO):
+    logging.basicConfig(format=msgfmt.format(prgname), level=level)
