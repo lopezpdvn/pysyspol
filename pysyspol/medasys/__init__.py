b8b048a7be328419f6c0ad59273983b1ff8e43bf
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-09-13 00:12:40 -0500

    Added `pysyspol.medasys.validate_paths`

--- pysyspol/medasys/__init__.py
+++ pysyspol/medasys/__init__.py
@@ -1,5 +1,8 @@
 import json
 import logging
+from os import access, R_OK
+from os.path import join
+
 from pysyspol.util import get_script_name
 
 def get_core_resources(core_resources_fp):
@@ -14,3 +17,18 @@ def logging_config(prgname=None, msgfmt='[{0}]: %(message)s',
 def get_tagged_resources(resources, tags=()):
     return (resource for tag in tags for resource in resources
             if tag in resource['tags'])
+
+def validate_paths(resources, resources_fp):
+    valid = True
+    for resource in resources:
+        for path in resource['path']:
+            path = join(resources_fp, path)
+            if not access(path, R_OK):
+                logging.error('Resource `{}` not readable'.format(path))
+                valid = False
+    if valid:
+        logging.info('All resources exist and readable')
+    else:
+        logging.error('Some errors happened trying to read resources')
+
+    return valid
