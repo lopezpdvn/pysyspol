da87a1db2cc151693f4fd7580d9b659a50b2cce2
Pedro Ivan Lopez <m@pedroivanlopez.com>
2017-01-25 16:17:26 -0600

    Added function `pysyspol.process.is_remote_anki_desktop_active`

--- pysyspol/process.py
+++ pysyspol/process.py
@@ -4,6 +4,8 @@ from itertools import chain
 from os.path import join, exists
 from os import getpid, remove
 
+ANKI_DESKTOP_RE_PATTERN = r'.*python.*bin/anki'
+
 def is_local_unix_process_active(reobj, ps_cmd=('ps', 'aux')):
     with Popen(ps_cmd, stdout=PIPE, stderr=PIPE,
             universal_newlines=True) as proc:
@@ -20,7 +22,7 @@ def is_remote_unix_process_active(reobj, remote_id, ps_cmd=('ps', 'aux'),
                 if reobj.match(l)]
     return len(matched_processes) > 0
 
-def is_local_anki_desktop_active(pattern=r'.*python.*bin/anki',
+def is_local_anki_desktop_active(pattern=ANKI_DESKTOP_RE_PATTERN,
         ps_cmd=('ps', 'aux')):
     reobj = re.compile(pattern, re.IGNORECASE)
     return is_local_unix_process_active(reobj, ps_cmd)
@@ -28,8 +30,11 @@ def is_local_anki_desktop_active(pattern=r'.*python.*bin/anki',
 def is_remote_ankidroid_active(hostname, user=None,
         pattern=r'.*com.ichi2.anki.*', ps_cmd=('ps',)):
     reobj = re.compile(pattern, re.IGNORECASE)
-    remote_id = '@'.join(user, hostname) if user else hostname
+    remote_id = '@'.join([user, hostname]) if user else hostname
     return is_remote_unix_process_active(reobj, remote_id, ps_cmd)
+is_remote_anki_desktop_active = (
+    lambda hostname, username: is_remote_ankidroid_active(hostname,
+        username, pattern=ANKI_DESKTOP_RE_PATTERN, ps_cmd=('ps', 'aux')))
 
 def is_app_locked(root, name):
     lockf = join(root, 'var', 'lock', 'LCK..' + name)
