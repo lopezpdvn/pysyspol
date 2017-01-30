5f8774ea9a217e8d1703ee4a971bb50b6f60c7c1
Pedro Ivan Lopez <m@pedroivanlopez.com>
2017-01-29 19:07:08 -0600

    Variant of subprocess.call logs via logger.debug and logger.error
    Based on https://gist.github.com/bgreenlee/1402841

--- pysyspol/process.py
+++ pysyspol/process.py
@@ -1,4 +1,7 @@
 import re
+import select
+import sys
+from logging import DEBUG, ERROR
 from subprocess import Popen, PIPE
 from itertools import chain
 from os.path import join, exists
@@ -52,3 +55,37 @@ def lock_app(root, name, lock=True):
         remove(lockf)
         return lockf
     return False
+
+def call(popenargs, logger, stdout_log_level=DEBUG, stderr_log_level=ERROR,
+        sys_stderr=True, **kwargs):
+    """
+    Variant of subprocess.call that accepts a logger instead of stdout/stderr,
+    and logs stdout messages via logger.debug and stderr messages via
+    logger.error.
+
+    Based on https://gist.github.com/bgreenlee/1402841
+    """
+    child = Popen(popenargs, stdout=PIPE, stderr=PIPE, **kwargs)
+
+    log_level = {child.stdout: stdout_log_level,
+                 child.stderr: stderr_log_level}
+
+    def check_io():
+        ready_to_read = select.select([child.stdout, child.stderr],
+                [], [], 1000)[0]
+        for io in ready_to_read:
+            line = io.readline()
+            linestr = line[:-1].decode('UTF-8')
+            if not linestr:
+                continue
+            logger.log(log_level[io], linestr)
+            if io is child.stderr and sys_stderr:
+                print(linestr, file=sys.stderr)
+
+    # keep checking stdout/stderr until the child exits
+    while child.poll() is None:
+        check_io()
+
+    check_io()  # check again to catch anything after the process exits
+
+    return child.wait()
