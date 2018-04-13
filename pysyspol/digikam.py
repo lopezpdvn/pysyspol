7c0e956d7ea2a7985313da4d7583c65c2e3a0f4d
Pedro Ivan Lopez <m@pedroivanlopez.com>
2018-04-12 22:31:30 -0500

    Added SQL query to search by tags

--- pysyspol/digikam.py
+++ pysyspol/digikam.py
@@ -2,6 +2,25 @@ import sqlite3
 from itertools import chain
 
 SQL_SELECT_TAGS = "SELECT Name FROM Tags WHERE pid != 1"
+
+SQL_SEARCHED_TAGGED = '''\
+SELECT DISTINCT ar.specificPath
+             || a.relativepath
+             || '/'
+             || i.name
+  'relpath'
+FROM Tags t
+  INNER JOIN ImageTags it
+    ON t.id = it.tagid
+  INNER JOIN Images i
+    ON i.id = it.imageid
+  INNER JOIN Albums a
+    ON i.album = a.id
+  INNER JOIN AlbumRoots ar
+    ON ar.id = a.albumRoot
+WHERE LOWER(t.Name) IN (?)\
+'''
+
 EXCLUDE_DIGIKAM_TAGS = set(('_Digikam_Internal_Tags_',))
 
 def get_tags(dbfp, exclude=EXCLUDE_DIGIKAM_TAGS):
